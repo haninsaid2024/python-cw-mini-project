ed0b9f89e1f89bd53ee967449ebc7732bb7c8039
haninsaid2024 <haninfouda077@gmail.com>
2023-09-05 21:59:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ def  padel_court_cost(court_type):
     elif court_type == "outdoor":
          return 20
 
-print(padel_court_cost ("indoor"))
 
 
 
@@ -19,24 +18,32 @@ def rackets_cost(racket_brand):
     
     elif  rackets_cost == "Siux" :
        return 200
-    
-print(rackets_cost("Siux")) 
 
 
 
 
-def padel_balls_cost(ball_boxes):
+def padel_ball_cost(ball_boxes):
     if ball_boxes == 1 :
-      return 2
+        cost = 2
     
     elif ball_boxes == 2:
        
-       return 3.5
+       cost = 3.5
     
     elif ball_boxes == 3:
         
-        return 5 
-        
+        cost =  5 
+    
+   
+def padel_game_cost():
+    
+     court_type = input("court type indoor / outdoor")
+     rackets_brand = input("racket_brand : nox / sulux / bullpade")
+     ball_boxes = int(input("number of ball boxes: 1 / 2 / 3"))
+     print = padel_court_cost(court_type) + rackets_cost(rackets_brand) + padel_ball_cost(ball_boxes)
+     return print
+
+print(padel_game_cost())
 
 
    
