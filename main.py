57d54e0ab90106b5e40058b31b0a4bd0648a47ea
haninsaid2024 <haninfouda077@gmail.com>
2023-09-05 14:25:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
-# write your code here
+def  padel_court_cost(court_type):
+    if court_type == "indoor" :
+       return 30
+    
+    
+    elif court_type == "outdoor":
+         return 20
+
+print(padel_court_cost ("indoor"))
+
+
+
+def rackets_cost(racket_brand):
+    if racket_brand == "bullpade":
+       return 100
+    
+    elif rackets_cost == "Nox" :
+        return 140
+    
+    elif  rackets_cost == "Siux" :
+       return 200
+    
+print(rackets_cost("Siux")) 
+
+
+
+
+def padel_balls_cost(ball_boxes):
+    if ball_boxes == 1 :
+      return 2
+    
+    elif ball_boxes == 2:
+       
+       return 3.5
+    
+    elif ball_boxes == 3:
+        
+        return 5 
+        
+
+
+   
+
+
